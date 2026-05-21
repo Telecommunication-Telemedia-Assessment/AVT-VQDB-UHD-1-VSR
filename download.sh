545c5a91dcacac4b82de583ce6059c46492b0d83
Benjamin Herb <github@benjaminherb.de>
2026-05-21 11:46:09 +0200

    Updated download script

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 base_url="https://avtshare01.rz.tu-ilmenau.de/avt-vqdb-uhd-1-vsr"
 
-wget -q --show-progress -r -l1 -np -nH --cut-dirs=1 -R "*.html*" -A "*.mkv" -c -P . "${base_url}/original/"
-wget -q --show-progress -r -l1 -np -nH --cut-dirs=1 -R "*.html*" -A "*.mkv" -c -P . "${base_url}/sources/"
-wget -q --show-progress -r -l1 -np -nH --cut-dirs=1 -R "*.html*" -A "*.mkv" -c -P . "${base_url}/pvs/"
+wget -q --show-progress -r -l1 -np -nH --cut-dirs=1 -R "*.html*" -A "*.mkv" -c -P . "${base_url}/reference/"
+wget -q --show-progress -r -l1 -np -nH --cut-dirs=1 -R "*.html*" -A "*.mkv" -c -P . "${base_url}/degraded/"
+wget -q --show-progress -r -l1 -np -nH --cut-dirs=1 -R "*.html*" -A "*.mkv" -c -P . "${base_url}/upscaled/"
